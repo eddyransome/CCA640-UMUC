3d307aeec92ca53d6ee13d3e8da99285015bfb7b
Eddy Ransome <eddyransome@aol.com>
2020-01-19 19:34:18 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# CCA640-UMUC
+# ballotonline-webapp
+
+This project is project is intended for UMUC/UMGC student practice only!
+
+It is not licensed under any regulation. Use at your own risk
+
+Author: Edmond Pewing, UMUC Graduate Student.
+Title: Sr.DevOps Engineer  
+
---
+++ bo-webapp/index.php
@@ -0,0 +1,9 @@
+<html><head><title><?php echo "Welcome to BallotOnline Voting Application";?></title></head>
+<body
+<?php 
+$image_url='https://edlyntech-public.s3.amazonaws.com/og-register-to-vote.jpg';
+?>
+
+<img src="<?php echo $image_url;?>">
+
+<br><h1>Help Change America by Voting! Every Vote Counts.</h1>
